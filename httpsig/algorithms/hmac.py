e64ee293ec0a1a73db670779c1beadf4563df8df
Bryan Zegar <bryanzegar@gmail.com>
2014-12-24 13:13:53 -0500

    Added algorithm class HMACSHA1.

--- httpsig/algorithms/hmac.py
+++ httpsig/algorithms/hmac.py
@@ -1,7 +1,29 @@
-from Crypto.Hash import HMAC, SHA256
+from Crypto.Hash import HMAC, SHA, SHA256
 import six
 
 from .base import BaseHMACAlgorithm
+
+
+class HMACSHA1(BaseHMACAlgorithm):
+    algorithm_name = 'hmac'
+    hash_name = 'sha1'
+
+    def __init__(self, secret):
+        """
+        An algorithm to sign messages with HMAC using SHA-1.
+
+        Args:
+          secret (bytes): A shared HMAC secret.
+
+        Example Usage:
+        >>> secret = b'secret'
+        >>> from httpsig.algorithms.hmac import HMACSHA1
+        >>> HMACSHA1(secret).create_signature('Message')
+        'gPQlziBEGEZE+Fq7plW/qziLdqc='
+        """
+        self.secret = secret
+        self._algorithm = HMAC
+        self._hash_method = SHA
 
 
 class HMACSHA256(BaseHMACAlgorithm):

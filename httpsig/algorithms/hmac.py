569404540175431528d51c1d2943ed734a183ea0
Bryan Zegar <bryanzegar@gmail.com>
2014-12-24 13:23:07 -0500

    Added algorithm HMACSHA512.

--- httpsig/algorithms/hmac.py
+++ httpsig/algorithms/hmac.py
@@ -1,4 +1,4 @@
-from Crypto.Hash import HMAC, SHA, SHA256
+from Crypto.Hash import HMAC, SHA, SHA256, SHA512
 import six
 
 from .base import BaseHMACAlgorithm
@@ -46,3 +46,26 @@ class HMACSHA256(BaseHMACAlgorithm):
         self.secret = secret
         self._algorithm = HMAC
         self._hash_method = SHA256
+
+
+class HMACSHA512(BaseHMACAlgorithm):
+    algorithm_name = 'hmac'
+    hash_name = 'sha512'
+
+    def __init__(self, secret):
+        """
+        An algorithm to sign messages with HMAC using SHA-256.
+
+        Args:
+          secret (bytes): A shared HMAC secret.
+
+        Example Usage:
+        >>> secret = b'secret'
+        >>> from httpsig.algorithms.hmac import HMACSHA512
+        >>> HMACSHA512(secret).create_signature('Message')
+        'z12KOCXYQjCZyKf6WP+yYBONCS+IwNuv9oPbRcL4u+WetE4B' + \
+        'vAm1Ysy+bEyGxq/QDLAufO0sPnVLUl/ubvPGdQ=='
+        """
+        self.secret = secret
+        self._algorithm = HMAC
+        self._hash_method = SHA512
